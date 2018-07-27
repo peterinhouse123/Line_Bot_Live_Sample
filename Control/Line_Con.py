33b5fdc4174eab88c3bd57532803029e5c10e894
peterinhouse <peterinhouse123@gmail.com>
2018-07-28 06:05:28 +0800

    'send_msg_done'

--- Control/Line_Con.py
+++ Control/Line_Con.py
@@ -1,10 +1,14 @@
 from Module import Browser
 from Module import Browser as Browser_Module
+
+from selenium.webdriver.common.keys import Keys
 import time
 class Line:
     def __init__(self):
         self.Browser_fn = Browser_Module.Browser()
         self.Driver = self.Browser_fn.Init_Browser("../")
+        self.Friend_List = []
+
 
     def Start_Line(self):
         self.Driver.get("chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html")
@@ -22,6 +26,47 @@ class Line:
         #判斷手機驗証碼是否通過了
         self.Check_Mobile_Captcha_Screen()
         print("驗証碼通過了哦")
+        time.sleep(3)
+
+        self.Get_Friend_List()
+        self.Send_Mesg_To_Friend(mid="u46710a7f314832fce484bf63bd0eaf4f",msg_body="hello")
+
+
+    def Send_Mesg_To_Friend(self,mid,msg_body):
+        self.Click_Firend_Tab()
+        Friend_ele = self.Driver.find_element_by_xpath('//li[@data-mid="'+format(mid)+'"]')
+        Friend_ele.click()
+
+        Chat_Input = self.Driver.find_element_by_css_selector("#_chat_room_input")
+        Chat_Input.send_keys(msg_body)
+        Chat_Input.send_keys(Keys.ENTER)
+
+
+
+    def Click_Firend_Tab(self):
+        Friends_btn = self.Driver.find_element_by_xpath('//li[@data-type="friends_list"]')
+        Friends_btn.click()
+        # time.sleep(1)
+
+    def Get_Friend_List(self):
+
+        Friend_list = self.Driver.find_elements_by_css_selector(".mdMN02Li")
+
+        end = []
+
+        for ele in Friend_list:
+            name = ele.find_element_by_css_selector(".mdCMN04Ttl").text
+            pack = {}
+            pack['ele'] = ele
+            pack['name'] = name
+            pack['mid'] = ele.get_attribute("data-mid")
+            end.append(pack)
+        self.Friend_List = end
+        # print(end)
+        return end
+
+
+
 
     def Check_Mobile_Captcha_Screen(self):
         Stat = 1
